98f82d2505c8c3556c5f2187e7165769c8066043
shehabic <shehabic@gmail.com>
2018-10-18 20:33:04 +0200

    Adding listener to the Android Plugin

--- android/src/main/java/com/deliveryhero/pandora/dhplugin/DhpluginPlugin.java
+++ android/src/main/java/com/deliveryhero/pandora/dhplugin/DhpluginPlugin.java
@@ -6,20 +6,41 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** DhpluginPlugin */
+/**
+ * DhpluginPlugin
+ */
 public class DhpluginPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "dhplugin");
-    channel.setMethodCallHandler(new DhpluginPlugin());
-  }
 
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.success("Android responded to " + call.method);
+    public interface Listener {
+        public void onDataRequested(String method, Result result);
     }
-  }
+
+    private static Listener listener;
+
+    public static void addListener(Listener listener) {
+        DhpluginPlugin.listener = listener;
+    }
+
+    public static void removeListener() {
+        listener = null;
+    }
+
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "dhplugin");
+        channel.setMethodCallHandler(new DhpluginPlugin());
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (listener != null) {
+            listener.onDataRequested(call.method, result);
+        } else {
+            result.notImplemented();
+        }
+    }
 }
